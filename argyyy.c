2b5a7c6073b8bb720e966b3255bf665db3477e98
argy18 <127745460+argy18@users.noreply.github.com>
2023-03-16 16:20:15 +0100

    Add files via upload

---
+++ argyyy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () 
+{
+    char stringa[]="";
+    int i=0;
+
+    while(1)
+    {
+        printf("inserire testo della stringa: ");
+        scanf("%s", stringa);
+        printf("%s\n", stringa);
+
+        if((stringa[0]=='S')&&(stringa[1]=='T')&&(stringa[2]=='O')&&(stringa[3]=='P'))
+        {
+            break;
+        }
+
+    } 
+
+} 
+ 
